2c198d161391f3dd924d2c4847339c64480afc9b
Andrew Caskey <caskeyandrew24@gmail.com>
2022-09-15 10:55:12 -0500

    gradeconverterimprovedp1

---
+++ GradeConverterImproved/GradeConverterImproved/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImprovedGradeConverter
+{
+    class Program
+    {
+
+        // Gets The User's First And Last Name
+        static string getName()
+        {
+            Console.Write("Please Enter Your First Name And Last Name: ");
+            string userName = Console.ReadLine();
+
+            return userName;
+        }
+
+        // Gets The Number Of Grades The User Wants To Enter.
+        static int getNumberOfGrades()
+        {
+            Console.Write("Enter The Number Of Grades You Need To Convert: ");
+            int numberOfGrades;
+            while (true)
+            {
+                try
+                {
+                    numberOfGrades = int.Parse(Console.ReadLine());
+                    return numberOfGrades;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Error: Only Numbers Allowed");
+                }
+            }
+        }
+
+        // Prints The Letter Grades And The Numerical Scores For The Grades Stored
+        static void printGradeReport(List<float> scores)
+        {
+            Console.WriteLine("\nGrade Report\n-------------------------");
+
+            // A = 90-100, B = 80-89, C = 70-79, D = 60 - 69, F Lower Than 60
+            foreach (float score in scores)
+            {
+                string letterGrade = getLetterGrade(score);
+
+                Console.WriteLine($"A Score Of {score} Is a '{letterGrade}' Grade");
+            }
+        }
+
+        // Prints The Statistics For The Grades List.
+        // (Number Of Grades, Average Grade, Maximum Grade, Minimum Grade)
+        static void printGradeStatistics(List<float> scores)
+        {
+            Console.WriteLine("\nGrade Statistics\n-------------------------");
+
+            float averageGrade = getAverageGrade(scores);
+            float maximumGrade = getMaximumGrade(scores);
+            float minimumGrade = getMinimumGrade(scores);
+
+            Console.WriteLine($"Number of grades: {scores.Count}");
+            Console.WriteLine($"Average Grade: {averageGrade} Which Is a '{getLetterGrade(averageGrade)}'");
+            Console.WriteLine($"Maximum Grade: {maximumGrade} Which Is a '{getLetterGrade(maximumGrade)}'");
+            Console.WriteLine($"Minimum Grade: {minimumGrade} Which Is a '{getLetterGrade(minimumGrade)}'");
+        }
+
+        // Calculates And Returns The Average Grade
+        static float getAverageGrade(List<float> scores)
+        {
+            float total = 0.0f;
+            foreach (float score in scores)
+            {
+                total += score;
+            }
+
+            return total / scores.Count;
+        }
+
+        // Returns The Highest Grade In The List Of Grades
+        static float getMaximumGrade(List<float> scores)
+        {
+            float max = float.MinValue;
+            foreach (float score in scores)
+            {
+                if (score > max)
+                {
+                    max = score;
+                }
+            }
+
+            return max;
+        }
+
+        // Returns The Lowest Grade In The List Of Grades
+        static float getMinimumGrade(List<float> scores)
+        {
+            float min = float.MaxValue;
+            foreach (float score in scores)
+            {
+                if (score < min)
+                {
+                    min = score;
+                }
+            }
+
+            return min;
+        }
+
+        static float getScore()
+        {
+            float grade;
+            while (true)
+            {
+                Console.Write("Enter The Score To Be Converted: ");
+                try
+                {
+                    grade = float.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Error: Only Numbers Allowed");
+                }
+            }
+            return grade;
+        }
+
+        static string getLetterGrade(float score)
+        {
+            //A = 90-100, B = 80-89, C = 70-79, D = 60 - 69, F lower than 60
+            if (score >= 90)
+            {
+                return "A";
+            }
+            else if (score >= 80)
+            {
+                return "B";
+            }
+            else if (score >= 70)
+            {
+                return "C";
+            }
+            else if (score >= 60)
+            {
+                return "D";
+            }
+            else
+            {
+                return "F";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\nGrade Converter\n-------------------------\n");
+
+            string userName = getName();
+            string doAgain;
+
+            Console.WriteLine($"\nHello {userName}!\nWelcome To The Grade Converter!\n");
+
+            do
+            {
+                // Get Number Of Grades
+                int numberOfGrades = getNumberOfGrades();
+                List<float> scores = new List<float>();
+
+                // Prompt The User To Enter The Grades.
+                Console.WriteLine("\nInput Grades\n-------------------------\n");
+                for (int i = 0; i < numberOfGrades; i++)
+                {
+                    scores.Add(getScore());
+                }
+
+                // Print Grade Report
+                printGradeReport(scores);
+
+                // Print Grade Statistics
+                printGradeStatistics(scores);
+                Console.WriteLine("-------------------------\n");
+
+                Console.Write("You You Like To Convert More Grades (Y/N)? ");
+                doAgain = Console.ReadLine().ToLower();
+            } while (doAgain.Equals("y"));
+        }
+    }
+}
---
+++ Grade_Converter 2/Grade_Converter/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeConverter
+{
+    class Program
+    {
+        //this function is to take input of numbers to be graded
+        static float number()
+        {
+            float input;
+            try
+            {
+                Console.WriteLine("Enter the score to be converted");
+                input = float.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error: only numbers 1-100 allowed");
+                return -1;
+            }
+            return input;
+        }
+
+        //this function is to convert the number grade to letter grade
+        static char letterGrade(float numbers)
+        {
+            char letter = '\0'; //
+
+            if (numbers >= 90)
+            {
+                letter = 'A';
+            }
+
+            else if (numbers >= 80)
+            {
+                letter = 'B';
+            }
+
+            else if (numbers >= 70)
+            {
+                letter = 'C';
+            }
+
+            else if (numbers >= 60)
+            {
+                letter = 'D';
+            }
+       
+            else if (numbers < 60)
+            {
+                letter = 'F';
+            }
+
+            return letter;
+        }
+
+        //display grade average
+        static void grade_Average(int n, List<float> num)
+        {
+            float sum = 0;
+            foreach (float item in num)
+            {
+                sum += item;
+            }
+
+            float avg = sum / n;
+
+            Console.WriteLine($"Number of grade is {n} ");
+            Console.WriteLine($"Average grade: {avg}, which is a {letterGrade(avg)} ");
+        }
+
+        static void Main(string[] args)
+        {
+            while (true) // this while loop will iterate until user does not enters n or N to not enter the grade
+            {
+
+                Console.Write("Please enter your first and last name :: ");
+                string Name = Console.ReadLine();
+
+                Console.WriteLine($"Hello {Name} Welcome to the Grade Converter!");
+
+                Console.WriteLine("\nEnter the number of grades you need to convert: ");
+                int numAmount = Convert.ToInt32(Console.ReadLine());
+
+                List<float> numberGrades = new List<float>();
+
+                //this for loop iterates to take user input of numbers
+                for (int value = 0; value < numAmount; value++)
+                {
+
+                    //number function is called to input number grades
+                    float num = number();
+                    if (num >= 0.0 && num <= 100.0)
+                    {
+                        numberGrades.Add(num);
+                    }
+                    else if ((num < 0 || num > 100)) 
+                    {
+                        Console.WriteLine($"Grades are to be entered only between 0-100. Please enter again.");
+                        value--;
+                    }
+                }
+
+                // gets letter grade
+                foreach (float item in numberGrades)
+                {
+                    char gradeLetter = letterGrade(item); 
+                    Console.WriteLine($"A score of {item} is a {gradeLetter}");
+                }
+
+                Console.WriteLine("\n\nGrade Statistics");
+                Console.WriteLine("----------------------------------");
+
+                grade_Average(numAmount, numberGrades);
+
+                Console.WriteLine("Do you want to convert more grades ? y/n");
+                string choice = Console.ReadLine();
+                if (choice.ToUpper().Equals("N")) 
+                    break;
+            }
+        }
+    }
+}
+
+
