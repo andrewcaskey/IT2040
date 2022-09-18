8ec2100f0134d6dd4d87ba9bab5ac14f78217313
Andrew Caskey <caskeyandrew24@gmail.com>
2022-09-18 10:53:44 -0500

    09/18

---
+++ Document/Document/Program.cs
@@ -0,0 +1,69 @@
+﻿
+namespace Document
+{
+
+    class Document
+    {
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                Console.WriteLine("Document");
+
+                Console.WriteLine("Enter Document Title:");
+
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter Document Content:");
+
+                string str = Console.ReadLine();
+
+                string filename = name + ".txt";
+
+                string path = Environment.CurrentDirectory + "/" + filename;
+
+                if (!File.Exists(path))
+
+                {
+
+                    File.WriteAllText(path, str);
+
+                }
+
+                int l = 0;
+                int wrd = 1;
+                while (l <= str.Length - 1)
+                {
+                    /* check whether the current character is white space or new line or tab character*/
+                    if (str[l] == ' ' || str[l] == '\n' || str[l] == '\t')
+                    {
+                        wrd++;
+                    }
+
+                    l++;
+                }
+
+
+
+
+                Console.WriteLine(filename + " was successfully saved. The document contains " + wrd + " words");
+
+            }
+
+            catch (Exception e)
+
+            {
+
+                Console.WriteLine(e);
+
+            }
+
+        }
+
+    }
+
+}
+
---
+++ Notesmodule5/Notesmodule5/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace DictionaryDemo
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            {
+                //create dictionary containg car brands and models
+                Dictionary<string, string> carDictionary = new Dictionary<string, string>() {
+            { "Honda", "Accord" },
+            { "Toyota", "Camry" },
+            { "Ford", "Focus" },
+            { "Chevy", "Impala" }
+
+        };
+
+
+                //add items to dictionary 
+                carDictionary.Add("Hyundai", "Elantra");
+
+                //Print value from the dictionary 
+                Console.WriteLine(carDictionary["Honda"] + "\n");
+
+                //Loop throguh car dictionary 
+                foreach (KeyValuePair<string, string> entry in carDictionary)
+                {
+                    Console.WriteLine($"{entry.Key} makes the {entry.Value}");
+
+                }
+                Console.WriteLine();
+
+                foreach (string key in carDictionary.Keys)
+                {
+                    Console.WriteLine($"{key}: {carDictionary[key]}");
+                }
+                Console.WriteLine();
+
+                // check if key exists in the dictionary
+                string searchKey = "Honda";
+                if (carDictionary.ContainsKey(searchKey))
+                {
+                    Console.WriteLine($"{searchKey}: {carDictionary[searchKey]} exists");
+
+                }
+                else
+                {
+                    Console.WriteLine($"{searchKey} does not appear in the dictionary");
+                }
+
+            }
+
+
+        }
+
+    }
+
+}
