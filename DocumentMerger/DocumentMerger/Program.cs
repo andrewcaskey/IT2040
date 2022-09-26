5b4979bf8bb2256fa2d3b3431eece29ceff5e096
Andrew Caskey <caskeyandrew24@gmail.com>
2022-09-26 11:49:17 -0500

    somefiles

---
+++ DocumentMerger/DocumentMerger/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace DocumentMerger
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter First text File: ");
+                string file1 = Console.ReadLine();
+                while (!File.Exists(file1))
+                {
+                    Console.Write("File does not exist or cannot be found, try again: ");
+                    file1 = Console.ReadLine();
+                }
+
+                Console.Write("Enter Second File: ");
+                string file2 = Console.ReadLine();
+                while (!File.Exists(file2))
+                {
+                    Console.Write("File does not exist or cannot be found, try again: ");
+                    file2 = Console.ReadLine();
+                }
+
+
+
+                string newFile = file1.Split('.')[0] + file2.Split('.')[0] + ".txt";
+
+
+
+
+
+                string allText = System.IO.File.ReadAllText(file1);
+                allText += "\r\n";
+                allText += System.IO.File.ReadAllText(file2);
+
+                if (!File.Exists(newFile))
+                {
+                    FileStream fs = File.Create(newFile);
+                    fs.Close();
+                }
+                File.WriteAllText(newFile, allText);
+
+                Console.WriteLine("Content Written to '" + newFile + "'.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
+
